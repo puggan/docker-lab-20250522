30097e6a75cacb15688c8cabc2def2baafb2b3af
Thorbjörn Puggan Sundragon <puggan@sundragon.se>
2025-05-23 00:48:27 +0200

    auth jwt

--- auth/jwt.js
+++ auth/jwt.js
@@ -1,8 +1,12 @@
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
+const privateKey = fs.readFileSync('jwt-private.key', 'utf8');
+
 /**
  * @param {string} username
  * @param {boolean} mockExpired
  * @returns {string}
  */
 module.exports = function (username, mockExpired = false) {
-    return 'JWT-TOKEN-' + username;
+    return jwt.sign({username}, privateKey, {algorithm: 'RS256', expiresIn: mockExpired ? '-1h' : '15m'});
 }
