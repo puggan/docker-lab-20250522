9168dea992f5541e3b52190fda04e7cf83c3e1f7
Thorbjörn Puggan Sundragon <puggan@sundragon.se>
2025-05-22 22:42:45 +0200

    auth demo-database

---
+++ auth/makeDemoDb.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd -- "$(dirname -- "$0")"
+if [ -f "users.db" ]; then
+    exit 1
+fi
+sqlite3 users.db <<EOF
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL
+);
+
+INSERT INTO users (username, password) VALUES ('demo', 'demo');
+EOF
