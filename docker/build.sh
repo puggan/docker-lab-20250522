abb497d5d26935bf56f2c45a4a964683db708a36
Thorbjörn Puggan Sundragon <puggan@sundragon.se>
2025-05-23 02:58:45 +0200

    3th docker for auth

--- docker/build.sh
+++ docker/build.sh
@@ -3,5 +3,8 @@ set -e
 cd -- "$(dirname -- "$0")"
 cd ..
 
+auth/makeDemoDb.sh
+auth/makeKeyPair.sh
+docker build -t auth -f docker/auth/Dockerfile .
 docker build -t jokebox -f docker/jokebox/Dockerfile .
 docker build -t quotebox -f docker/quotebox/Dockerfile .
--- docker/start-bare.sh
+++ docker/start-bare.sh
@@ -2,5 +2,15 @@
 set -e
 cd -- "$(dirname -- "$0")"
 
-docker run -v $(realpath ../jokebox/jokes):/app/jokes -p 8082:8080 jokebox
-docker run -v $(realpath ../quotebox/quotes):/app/quotes -p 8083:8080 quotebox
+docker run -d \
+  -v $(realpath ../auth/jwt-private.key):/app/jwt-private.key \
+  -v $(realpath ../auth/users.db):/app/users.db \
+  -p 8081:8080 auth
+
+docker run -d \
+  -v $(realpath ../jokebox/jokes):/app/jokes \
+  -p 8082:8080 jokebox
+
+docker run -d \
+  -v $(realpath ../quotebox/quotes):/app/quotes \
+  -p 8083:8080 quotebox
