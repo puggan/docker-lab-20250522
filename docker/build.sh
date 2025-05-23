ad8a2a4b64b326240e54465a4eb0c477ea54ed3f
Thorbjörn Puggan Sundragon <puggan@sundragon.se>
2025-05-23 02:37:03 +0200

    2nd docker for jokebox in bash

--- docker/build.sh
+++ docker/build.sh
@@ -3,4 +3,5 @@ set -e
 cd -- "$(dirname -- "$0")"
 cd ..
 
+docker build -t jokebox -f docker/jokebox/Dockerfile .
 docker build -t quotebox -f docker/quotebox/Dockerfile .
--- docker/start-bare.sh
+++ docker/start-bare.sh
@@ -2,4 +2,5 @@
 set -e
 cd -- "$(dirname -- "$0")"
 
-docker run -v $(realpath ../quotebox/quotes):/app/quotes -p 8081:8080 quotebox
+docker run -v $(realpath ../jokebox/jokes):/app/jokes -p 8082:8080 jokebox
+docker run -v $(realpath ../quotebox/quotes):/app/quotes -p 8083:8080 quotebox
