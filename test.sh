07cf8b1e81c120e34fb35250eeb1cbff87460339
Thorbjörn Puggan Sundragon <puggan@sundragon.se>
2025-05-23 18:44:46 +0200

    test, also test the endpoints with an bad jwt

--- test.sh
+++ test.sh
@@ -3,7 +3,19 @@ set -x
 
 echo "Get JWT"
 JWT="$(curl -s "http://127.0.0.1:8080/auth" --data username=demo --data password=demo)"
+echo -e "\n\n\n"
 
 curl -s "http://127.0.0.1:8080/joke" --header "Authorization: JWT $JWT"
+echo -e "\n\n\n"
 
 curl -s "http://127.0.0.1:8080/quote" --header "Authorization: JWT $JWT"
+echo -e "\n\n\n"
+
+BAD_JWT="$(curl -s "http://127.0.0.1:8080/expired" --data username=demo --data password=demo)"
+echo -e "\n\n\n"
+
+curl -D - "http://127.0.0.1:8080/joke" --header "Authorization: JWT $BAD_JWT"
+echo -e "\n\n\n"
+
+curl -D - "http://127.0.0.1:8080/quote" --header "Authorization: JWT $BAD_JWT"
+echo -e "\n\n\n"
